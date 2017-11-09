34663a38a56e3e3ad46c23379bd7efd38688dd7b
godlike <gustavot53@gmail.com>
2017-11-09 18:48:33 -0200

    Adicionando conteudo ao layout

--- Views/Usuario/index.php
+++ Views/Usuario/index.php
@@ -1,3 +1,10 @@
+<?php 
+    require '../../Controllers/consultaGrafico.php';
+    
+    // Instancia e busca lista de anos registrados para preencher comboBox
+    $consulta = new consultaGrafico();
+    $dataset = $consulta->getListaAnos();
+?>
 <div style="margin-left: -100px;">
     <h1>Bem vindo <i><?=$_SESSION["login"]?></i>!</h1>
     <h4 class="text-muted"> Esta é sua pagina incial.</h4>
@@ -23,6 +30,14 @@
                 <img class="imagem-grafico" src="../Consulta/GraficoIdade.php" />
             </div>
             <div class="col-4 grafico">
+                <form action="#" name="filtraAno" method="post">
+                    <select name="anoGrafico">
+                        <?php while($ano = mysqli_fetch_array[$dataset]) {?>
+                        <option value="<?=$ano["Ano"]?>"><font>20</font><?=$ano["Ano"]?></option>
+                        <?php } ?>
+                    </select>
+                    <input type="submit" value="Atualizar" >
+                </form>
                 <img class="imagem-grafico" src="../Consulta/GraficoMensalAnual.php" />
             </div>
         </div>
