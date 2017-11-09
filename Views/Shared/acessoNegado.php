1b6d62ba8dc10b4c1d8fc8bd581f3f2896ca60cf
godlike <gustavot53@gmail.com>
2017-11-09 15:26:55 -0200

    Corrigido design

--- Views/Shared/acessoNegado.php
+++ Views/Shared/acessoNegado.php
@@ -2,10 +2,12 @@
 <?php if (isset($_SESSION["statusLogin"]) && $_SESSION["statusLogin"] == 1) { ?>
     <h4>A pagina solicitada não ser acessada nesta sessão.</h4>
     <hr />
+    <p>Parece que você não tem as permissoes ou requisitos para acessar está pagina.
+    <br />Recomendamos as seguintes ações:</p>
     <br />
     <div>
-        <a class="btn btn-outline-danger" href="../../Controllers/usuarioLogoff.php">Fazer LogOff</a>
-        <a class="btn btn-outline-primary" href="_Layout.php?CodPg=5">Voltar a Home</a>
+        <a class="btn btn-outline-danger" style="width: 15%;" href="../../Controllers/usuarioLogoff.php">Fazer LogOff</a>
+        <a class="btn btn-outline-primary" style="width: 15%;" href="_Layout.php?CodPg=5">Voltar a Home</a>
     </div>
 <?php } else { ?>
     <h4>A pagina solicitada não ser acessada sem estar logado!</h4>
