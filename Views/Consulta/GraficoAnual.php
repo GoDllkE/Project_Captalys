17311243ca7cf4e3af5c5cab026624fcd8996d1d
godlike <gustavot53@gmail.com>
2017-11-09 18:09:15 -0200

    Adicionando Views

---
+++ Views/Consulta/GraficoAnual.php
@@ -0,0 +1,72 @@
+<?php
+    // Requisitos
+    require_once '../../Controllers/consultaGrafico.php';
+    require_once '../../Content/libraries/phplot/phplot.php';
+
+    // Define array de dados a ser utilizado
+    $objConsulta = new consultaGrafico();
+    $dados = $objConsulta->geraValoresMediaAnual();
+    
+    // Define array de dados a ser utilizado
+    /*
+    $dados = array(
+        array('Janeiro', '10230', '11345'), 
+        array('Fevereiro', '12334', '8902'),
+        array('Marco', '12334', '8902'),
+        array('Abril', '11345', '8902'),
+        array('Maio', '8902', '8902'),
+        array('Junho', '10230', '11345')
+    );
+    */
+    
+    // Objeto
+    $MeuGrafico = new PHPlot(440, 250);
+    
+    // Borda
+    // $MeuGrafico->SetImageBorderType('plain');
+    
+    // Define o formato do arquivo da imagem
+    $MeuGrafico->SetFileFormat("png");
+
+    // Define o tipo de grafico, nesse caso em barras
+    $MeuGrafico->SetPlotType('bars');
+
+    // Define a fonte Padrão, nesse caso o arquivo ttf está no mesmo diretório
+    // $MeuGrafico->SetDefaultTTFont('Ubuntu.ttf'); 
+
+    // define se as barras serão em 3D, valor 0 Imagem chamada
+    $MeuGrafico->SetShading(0); 
+    $MeuGrafico->SetDataType('text-data');
+
+
+    //função para converter para real, pode haver outra maneira!
+    function converterParaReal($value) {
+        $deg = $value;
+        $real = number_format($deg,2,',','.');
+        return "R$ {$real}";
+    }
+
+    // setar o valores no eixo Y no formato moeda
+    // este metodo aceita uma função quando o parametro custom é setado
+    $MeuGrafico->SetYLabelType('custom', 'converterParaReal');
+
+    // seta os dados para a plotagem do grafico
+    $MeuGrafico->SetDataValues($dados);
+
+    // definição do titulo do gráfico
+    // por questão da acentuação utilizar a função utf8_decode
+    $titulo = utf8_decode('Valor médio em relação ao ano');
+
+    // Setar o titulo definido na varieavel $titulo anteriomente
+    $MeuGrafico->SetTitle($titulo);
+
+    // Gera uma legenda
+    $MeuGrafico->SetLegend(array('Ano', 'Valor médio'));
+
+    //Por padrão é setado "marcas" das escalas do eixo x, none retira estas marcas.
+    $MeuGrafico->SetXTickPos('none');
+
+    // gera o grafico
+    $MeuGrafico->DrawGraph();
+
+?> 
---
+++ Views/Consulta/GraficoMensal.php
@@ -0,0 +1,78 @@
+<?php
+    // Requisitos
+    require_once '../../Controllers/consultaGrafico.php';
+    require_once '../../Content/libraries/phplot/phplot.php';
+
+    // Define array de dados a ser utilizado
+    // $objConsulta = new consultaGrafico();
+    // $dados = $objConsulta->geraValoresMediaMes();
+    
+    // Define array de dados a ser utilizado
+    
+    $dados = array(
+        array('Janeiro', '10230'), 
+        array('Fevereiro', '12334'),
+        array('Marco', '12334'),
+        array('Abril', '11345'),
+        array('Maio', '8902'),
+        array('Junho', '10230'),
+        array('Julho', '10230'),
+        array('Agosto', '10230'),
+        array('Setembro', '10230'),
+        array('Outubro', '10230'),
+        array('Novembro', '10230'),
+        array('Dezembro', '10230'),
+    );
+    
+    
+    // Objeto
+    $MeuGrafico = new PHPlot(440, 250);
+    
+    // Borda
+    // $MeuGrafico->SetImageBorderType('plain');
+    
+    // Define o formato do arquivo da imagem
+    $MeuGrafico->SetFileFormat("png");
+
+    // Define o tipo de grafico, nesse caso em barras
+    $MeuGrafico->SetPlotType('bars');
+
+    // Define a fonte Padrão, nesse caso o arquivo ttf está no mesmo diretório
+    // $MeuGrafico->SetDefaultTTFont('Ubuntu.ttf'); 
+
+    // define se as barras serão em 3D, valor 0 Imagem chamada
+    $MeuGrafico->SetShading(0); 
+    $MeuGrafico->SetDataType('text-data');
+
+
+    //função para converter para real, pode haver outra maneira!
+    function converterParaReal($value) {
+        $deg = $value;
+        $real = number_format($deg,2,'.',',');
+        return "R$ {$real}";
+    }
+
+    // setar o valores no eixo Y no formato moeda
+    // este metodo aceita uma função quando o parametro custom é setado
+    $MeuGrafico->SetYLabelType('custom', 'converterParaReal');
+
+    // seta os dados para a plotagem do grafico
+    $MeuGrafico->SetDataValues($dados);
+
+    // definição do titulo do gráfico
+    // por questão da acentuação utilizar a função utf8_decode
+    $titulo = utf8_decode('Valor médio em relação ao mes');
+
+    // Setar o titulo definido na varieavel $titulo anteriomente
+    $MeuGrafico->SetTitle($titulo);
+
+    // Gera uma legenda
+    $MeuGrafico->SetLegend(array('Mes', 'Valor médio'));
+
+    //Por padrão é setado "marcas" das escalas do eixo x, none retira estas marcas.
+    $MeuGrafico->SetXTickPos('none');
+
+    // gera o grafico
+    $MeuGrafico->DrawGraph();
+
+?> 
