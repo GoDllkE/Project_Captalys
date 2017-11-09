3be5e0587fe9ab8282fc08a2b994779d8df59f48
godlike <gustavot53@gmail.com>
2017-11-09 15:22:18 -0200

    Adicionado mensagem informativa e design reformulado

--- Views/Shared/acessoNegado.php
+++ Views/Shared/acessoNegado.php
@@ -4,15 +4,19 @@
     <hr />
     <br />
     <div>
-        <a href="../../Controllers/usuarioLogoff.php">Fazer LogOff</a>
-        <a href="_Layout.php?CodPg=5">Voltar a Home</a>
+        <a class="btn btn-outline-danger" href="../../Controllers/usuarioLogoff.php">Fazer LogOff</a>
+        <a class="btn btn-outline-primary" href="_Layout.php?CodPg=5">Voltar a Home</a>
     </div>
 <?php } else { ?>
     <h4>A pagina solicitada não ser acessada sem estar logado!</h4>
     <hr />
+    <p>Parece que você não tem as permissoes ou requisitos para acessar está pagina.
+    <br />Recomendamos as seguintes ações:</p>
     <br />
     <div>
-        <a href="_Layout.php?CodPg=3">Fazer Login</a>
-        <a href="_Layout.php">Voltar a Home</a>
+        <center>
+        <a class="btn btn-outline-success" style="width: 15%;" href="_Layout.php?CodPg=3">Fazer Login</a>
+        <a class="btn btn-outline-primary" style="width: 15%;" href="_Layout.php">Voltar a Home</a>
+        </center>
     </div>
 <?php } ?>
