906d8606980bf3bb08e5d7dc91d5ad4838ffffb2
godlike <gustavot53@gmail.com>
2017-11-09 16:39:00 -0200

    Atualizando estrutura e design da pagina

--- Views/Usuario/index.php
+++ Views/Usuario/index.php
@@ -1,14 +1,23 @@
-<!-- Colocar gráfico aqui -->
+<div style="margin-left: -100px;">
+    <h1>Bem vindo <i><?=$_SESSION["login"]?></i>!</h1>
+    <h4 class="text-muted"> Esta é sua pagina incial.</h4>
+    <hr />
+    <p> 
+        Aqui você pode conferir gráficos do status atual do banco de dados!
+        <br /> Dê uma olhada:
+    </p>
+    <br />    
+</div>
 <fieldset>
         <div class="row">
             <div class="col-4 grafico" style="margin-left: -100px;">
-                <img class="imagem-grafico" src="#" />
+                <img class="imagem-grafico" src="../Consulta/Grafico1.php" />
             </div>
             <div class="col-4 grafico">
-                <img class="imagem-grafico" src="#" />
+                <img class="imagem-grafico" src="../Consulta/Grafico2.php" />
             </div>
             <div class="col-4 grafico">
-                <img class="imagem-grafico" src="#" />
+                <img class="imagem-grafico" src="../Consulta/Grafico3.php" />
             </div>
         </div>
 </fieldset>
