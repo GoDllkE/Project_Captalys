66a2bca2d73605c6f986e20199c003cfe7eb58b2
godlike <gustavot53@gmail.com>
2017-11-09 16:23:52 -0200

    Atualizando layout dos graficos

--- Views/Usuario/index.php
+++ Views/Usuario/index.php
@@ -1,4 +1,14 @@
 <!-- Colocar gráfico aqui -->
 <fieldset>
-    <?php include './criaGrafico.php'; ?>
-</fieldset>  
+        <div class="row">
+            <div class="col-4 grafico" style="margin-left: -100px;">
+                <img class="imagem-grafico" src="#" />
+            </div>
+            <div class="col-4 grafico">
+                <img class="imagem-grafico" src="#" />
+            </div>
+            <div class="col-4 grafico">
+                <img class="imagem-grafico" src="#" />
+            </div>
+        </div>
+</fieldset>
