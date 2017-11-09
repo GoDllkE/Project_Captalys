a0d8baacbe119d0a19d31139cf704547f94121e9
godlike <gustavot53@gmail.com>
2017-11-09 17:06:01 -0200

    Adicionado estrutura DAO

--- DAO/ConsultaDAO.php
+++ DAO/ConsultaDAO.php
@@ -3,6 +3,50 @@
     require_once '../Models/Consulta.php';
     
     class ConsultaDAO {
+     
+        function listaValoresAnuais() {
+            // Objetos
+            $conn = new Conexao();
+            $conector = $conn->abreConexao();
+            
+            $sql = "SELECT ano, avg(valor) FROM consultas GROUP BY ano";
+            $dataset = mysqli_query($conector, $sql);
+            
+            return $dataset;
+        }
+        
+        function listaValoresIdade() {
+            // Objetos
+            $conn = new Conexao();
+            $conector = $conn->abreConexao();
+            
+            $sql = "SELECT idade, avg(valor) FROM consultas GROUP BY idade";
+            $dataset = mysqli_query($conector, $sql);
+            
+            return $dataset;
+        }
+        
+        function listaValoresMes() {
+            // Objetos
+            $conn = new Conexao();
+            $conector = $conn->abreConexao();
+            
+            $sql = "SELECT idade, avg(valor) FROM consultas GROUP BY idade";
+            $dataset = mysqli_query($conector, $sql);
+            
+            return $dataset;
+        }
+        
+        function listaValoresMensalAnual($ano) {
+            // Objetos
+            $conn = new Conexao();
+            $conector = $conn->abreConexao();
+            
+            $sql = "SELECT mes, avg(valor) FROM consultas WHERE ano=".$ano." GROUP BY mes;";
+            $dataset = mysqli_query($conector, $sql);
+            
+            return $dataset;
+        }
         
     }
 ?>
