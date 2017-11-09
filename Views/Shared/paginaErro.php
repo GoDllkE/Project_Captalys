09c6910587172947d0cdcab9798d090c2207e0f2
godlike <gustavot53@gmail.com>
2017-11-09 15:12:12 -0200

    Adicionando mensagem e link de retorno

--- Views/Shared/paginaErro.php
+++ Views/Shared/paginaErro.php
@@ -1,8 +1,9 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h1>Pagina não encontrada!</h1>
+<h4>A pagina solicitada não se encontra disponivel no momento.</h4>
+<hr />
+<br />
+<?php if (isset($_SESSION["statusLogin"]) && $_SESSION["statusLogin"] == 1) { ?>
+    <a href="_Layout.php?CodPg=5">Voltar a Home</a>
+<?php } else { ?>
+    <a href="_Layout.php">Voltar a Home</a>
+<?php } ?>
