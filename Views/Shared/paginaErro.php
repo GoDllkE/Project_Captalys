a2413fd323d265c6dc6c79703808450682b6e824
godlike <gustavot53@gmail.com>
2017-11-09 15:26:04 -0200

    Adicionado mensagem informativa e melhoria no design

--- Views/Shared/paginaErro.php
+++ Views/Shared/paginaErro.php
@@ -1,9 +1,14 @@
 <h1>Pagina não encontrada!</h1>
-<h4>A pagina solicitada não se encontra disponivel no momento.</h4>
+<h4> A pagina solicitada não se encontra disponivel no momento.</h4>
 <hr />
+<p> Parece que você está tentando acessar uma pagina que não existe ou está em processo
+    de manutenção...
+<br />Recomendamos as seguintes ações:</p>
 <br />
-<?php if (isset($_SESSION["statusLogin"]) && $_SESSION["statusLogin"] == 1) { ?>
-    <a href="_Layout.php?CodPg=5">Voltar a Home</a>
+<center>
+<?php if (isset($_SESSION["statusLogin"]) && $_SESSION["statusLogin"] == 1) { ?>    
+    <a class="btn btn-outline-primary" style="width: 20%;" href="_Layout.php?CodPg=5">Voltar para Home</a>
 <?php } else { ?>
-    <a href="_Layout.php">Voltar a Home</a>
+    <a class="btn btn-outline-primary" style="width: 20%;" href="_Layout.php">Voltar para Home</a>
 <?php } ?>
+</center>
