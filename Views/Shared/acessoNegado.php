3c743efe1b2db72d48ff47ef274e54f968f284d9
godlike <gustavot53@gmail.com>
2017-11-09 15:14:45 -0200

    Adicionado conteudo a pagina e link para redirecionamento

--- Views/Shared/acessoNegado.php
+++ Views/Shared/acessoNegado.php
@@ -1,8 +1,18 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<h1>Acesso negado!</h1>
+<?php if (isset($_SESSION["statusLogin"]) && $_SESSION["statusLogin"] == 1) { ?>
+    <h4>A pagina solicitada não ser acessada nesta sessão.</h4>
+    <hr />
+    <br />
+    <div>
+        <a href="../../Controllers/usuarioLogoff.php">Fazer LogOff</a>
+        <a href="_Layout.php?CodPg=5">Voltar a Home</a>
+    </div>
+<?php } else { ?>
+    <h4>A pagina solicitada não ser acessada sem estar logado!</h4>
+    <hr />
+    <br />
+    <div>
+        <a href="_Layout.php?CodPg=3">Fazer Login</a>
+        <a href="_Layout.php">Voltar a Home</a>
+    </div>
+<?php } ?>
