ccbdac8eae911f7e494e51dcb9311198c919193f
godlike <gustavot53@gmail.com>
2017-11-09 18:29:17 -0200

    Atualizando aqruivos opicionais

--- Views/Home/index.php
+++ Views/Home/index.php
@@ -1,1 +1,46 @@
-<h1>Home Index</h1>
+<div id="parallax1" class="bgParallax" data-speed="10">
+    <article>
+        <center>
+            <h1>Bem vindo!</h1>
+            <p>Este é um site inutil</p>
+        </center>
+    </article>
+</div>
+<ol class="">
+    <li id="parallax2" class="bgParallax" data-speed="10">
+        <article>
+            <h2>Getting Started</h2>
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dolor magna, 
+            facilisis quis porttitor sit amet, maximus sed odio. Phasellus viverra consectetur 
+            vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur 
+            ridiculus mus. Vestibulum pellentesque neque sed consequat congue. 
+            Sed porttitor est sit amet justo malesuada, sit amet laoreet sapien porta. 
+            <a href="http://www.pudim.com.br/" style="color: crimson;">Learn more…</a>
+        </article>
+    </li>
+    <li id="parallax3" class="bgParallax">
+        <article>
+            <h2>Aenean dignissim urna ut pellentesque imperdiet.</h2>
+            <br />
+            Maecenas finibus vestibulum orci, eu facilisis dui iaculis ac. Vivamus hendrerit, 
+            arcu eget pretium lacinia, diam tortor tristique urna, vitae dapibus nibh risus 
+            nec ante. Suspendisse vitae urna vestibulum, ullamcorper tortor vestibulum, 
+            scelerisque tellus.
+            <a href="http://www.pudim.com.br/" style="color: yellow;">Learn more…</a>
+        </article>
+    </li>
+    <li id="parallax4" class="bgParallax" data-speed="10">
+        <article>
+            <h2>Vivamus nec gravida odio</h2>
+            <br />
+            Quisque nisi sapien, tincidunt quis bibendum eleifend, 
+            congue ut risus. Praesent pulvinar elementum porttitor. In porta a arcu at 
+            sollicitudin.
+            <a href="http://www.pudim.com.br/" style="color: cyan">Learn more…</a>
+        </article>
+    </li>
+</ol>
+
+<!-- Carregamento do Parallax -->
+<script src="~/Content/js/Parallax.js"></script>
--- Views/Home/sobre.php
+++ Views/Home/sobre.php
@@ -1,1 +1,43 @@
-<h1>Sobre</h1>
+<hgroup class="title">
+    <h1>Sobre</h1>
+    <h4 class="muted-text">Uma breve descrição das coisas</h4>
+</hgroup>
+<hr />
+<article>
+    <p>
+        O maior desafio deste projeto foi o tempo, tive muito pouco tempo para desenvolver 
+        ele (semana corrida com compromissos). Começei o desenvolvimento deste na quinta-feira 
+        na parte da manhã.
+    </p>
+    <p>
+        Para este, estou utilizando o PHPlot da qual é gratuito para geração de graficos,
+        que agora (quinta-feira, 18:18) não estão funcionando devido ao php retornar o
+        seguinte erro: 
+        <br />
+        <br />
+        <font size="4pt" class="text-muted">
+            <i>
+                PHP Fatal error:  require_once(): Failed opening required 
+                '../DAO/ConsultaDAO.php' (include_path='.:') in 
+                /srv/http/project_captalys/Controllers/consultaGrafico.php on line 3
+            </i>
+        </font>
+        <br />
+        <br />
+        Vou tentar solucionar este a tempo, acredito que possa ser alguma config do meu
+        servidor web referente ao virtual servers. O ruim é que não posso testar 
+        os gráficos, se os dataset enviado para eles esta ok para exibir um gráfico...
+    </p>
+    <p>
+        Mesmo que não consiga passar, consegui aprender umas coisas legais em PHP em no pouco 
+        tempo que tive, que fez valer a pena. Foi bem corrido mas é assim mesmo. 
+        <br />Segue o jogo!
+    </p>
+</article>
+
+<aside>
+    <h3>Observação</h3>
+    <p>
+        Não consigo ler nada.
+    </p>
+</aside>
